36c6359fe897f0ecb2b84076a342cd3f62ee6f83
JYS <jys60mlb@gmail.com>
2023-08-09 21:42:03 +0900

    홈페이지 수정중

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,217 +1,463 @@
 <script lang="ts">
-    let today = new Date();   
-    let year = today.getFullYear(); // 년도
-    let month = today.getMonth() + 1;  // 월
-    let date = today.getDate();  // 날짜
-    let day = today.getDay();  // 요일
+    import { browser } from "$app/environment";
+    import { Datepicker } from "svelte-calendar";
+    import dayjs from "dayjs";
+
+    const theme = {
+        calendar: {
+            width: "500px",
+            legend: {
+                height: "45px",
+            },
+            shadow: "0px 0px 5px rgba(0, 0, 0, 0.25)",
+            colors: {
+                background: {
+                    highlight: "purple",
+                },
+            },
+            font: {
+                regular: "1em",
+                large: "22em",
+            },
+        },
+    };
+
+    let format = "YYYY-MM-DD";
+
+    const DefaultSelectedDate = new Date(2023, 5, 12); // 월은 0부터 시작
+    const DateOptions = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+
+    let store;
+    let selecteddate;
+
+    $: {
+        selecteddate = dayjs($store?.selected).format(format);
+        GetSchedule();
+        GetRankFromAPI();
+    }
+
+    let schedulearray = [];
+
+    async function GetSchedule() {
+        // As I load the data initially with PageServerLoad (https://kit.svelte.dev/docs/load#page-data), I for my part do not need to do a fetch in a svelte component while it is rendered server side. But I do need to do a fetch later on, which will only be on client side. My solution was to wrap it with the browser environment variable from sveltekit.
+
+        // import { browser } from "$app/environment";
+
+        // if (browser) {
+        // const res = await fetch(
+        //     // url
+        // );
+        // data = await res.json();
+        // }
+        // With this I did not get the error "Cannot use relative URL (...) with global fetch".
+
+        // Just writing it as this may help some others reading this.
+
+        if (!browser) {
+            return;
+        }
+
+        const response = await fetch(
+            `home/db/getschedule?SelectedDate=${selecteddate}`
+        );
+        //const response = await fetch(`teamranking/api?Conference=${'East'}&Season=${'2022-23'}&SeasonType=${'Regular Season'}`);
+        schedulearray = await response.json();
+
+        console.log(schedulearray);
+    }
+
+    let RankAPIData: (number | string)[][];
+
+    let RankArray_PTS = [];
+    let RankArray_REB = [];
+    let RankArray_AST = [];
+    let RankArray_BLK = [];
+    let RankArray_STL = [];
+    let RankArray_FANTASY_PTS = [];
+
+    function CompareDescending(a: (number | string)[], b: (number | string)[]) {
+        if (a[0] < b[0]) {
+            return -1;
+        } else if (a[0] > b[0]) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    async function GetRankFromAPI() {
+        if (!browser) {
+            return;
+        }
+
+        const response = await fetch(
+            `home/api/leaguedashplayerstats?DateFrom=${selecteddate}&DateTo=${selecteddate}`
+        );
+
+        RankAPIData = await response.json();
+
+        let tempRankArray_PTS = [];
+        let tempRankArray_REB = [];
+        let tempRankArray_AST = [];
+        let tempRankArray_BLK = [];
+        let tempRankArray_STL = [];
+        let tempRankArray_FANTASY_PTS = [];
+
+        for (let i = 0; i < RankAPIData.length; i++) {
+            //  PTS_RANK
+            if (RankAPIData[i][5] <= 5) {
+                tempRankArray_PTS.push([
+                    RankAPIData[i][5],
+                    RankAPIData[i][1],
+                    RankAPIData[i][3],
+                    RankAPIData[i][4],
+                ]);
+            }
+            //  REB_RANK
+            if (RankAPIData[i][7] <= 5) {
+                tempRankArray_REB.push([
+                    RankAPIData[i][7],
+                    RankAPIData[i][1],
+                    RankAPIData[i][3],
+                    RankAPIData[i][6],
+                ]);
+            }
+            //  AST_RANK
+            if (RankAPIData[i][9] <= 5) {
+                tempRankArray_AST.push([
+                    RankAPIData[i][9],
+                    RankAPIData[i][1],
+                    RankAPIData[i][3],
+                    RankAPIData[i][8],
+                ]);
+            }
+            //  BLK_RANK
+            if (RankAPIData[i][11] <= 5) {
+                tempRankArray_BLK.push([
+                    RankAPIData[i][11],
+                    RankAPIData[i][1],
+                    RankAPIData[i][3],
+                    RankAPIData[i][10],
+                ]);
+            }
+            //  STL_RANK
+            if (RankAPIData[i][13] <= 5) {
+                tempRankArray_STL.push([
+                    RankAPIData[i][13],
+                    RankAPIData[i][1],
+                    RankAPIData[i][3],
+                    RankAPIData[i][12],
+                ]);
+            }
+            //  NBA_FANTASY_PTS_RANK
+            if (RankAPIData[i][15] <= 5) {
+                tempRankArray_FANTASY_PTS.push([
+                    RankAPIData[i][15],
+                    RankAPIData[i][1],
+                    RankAPIData[i][3],
+                    RankAPIData[i][14],
+                ]);
+            }
+        }
+
+        RankArray_PTS = tempRankArray_PTS.sort(CompareDescending).slice(0, 5);
+        RankArray_REB = tempRankArray_REB.sort(CompareDescending).slice(0, 5);
+        RankArray_AST = tempRankArray_AST.sort(CompareDescending).slice(0, 5);
+        RankArray_BLK = tempRankArray_BLK.sort(CompareDescending).slice(0, 5);
+        RankArray_STL = tempRankArray_STL.sort(CompareDescending).slice(0, 5);
+        RankArray_FANTASY_PTS = tempRankArray_FANTASY_PTS.sort(CompareDescending).slice(0, 5);
+
+        //console.log(RankAPIData);
+    }
 </script>
 
-<div>
-    <h1 class="date">{year + '년 ' + month + '월 ' + date + '일'}</h1>
+<body>
+    <img src="/home_image01.jpg" alt="homeimage01" class="homeimage01" />
 
-    <div>
-        <table class="week">
-            <tr class="border">
-                <td class="day sunday">일</td>
-                <td class="day">월</td>
-                <td class="day">화</td>
-                <td class="day">수</td>
-                <td class="day">목</td>
-                <td class="day">금</td>
-                <td class="day saturday">토</td>
-            </tr>
-        </table>
+    <div class="datepickercontainer">
+        <Datepicker
+            bind:store
+            selected={DefaultSelectedDate}
+            {theme}
+            {format}
+        />
+        <!-- 
+        - 달력이 Datepicker 아랫쪽에서 나오게 변경
+        - 달력의 날짜칸에 경기 수가 함께 표시되게 수정
+        - 가능하다면 경기 수가 0인 날짜는 클릭 안되게 처리
+        -->
     </div>
-    
-    <div class="mvptitle">오늘의 MVP</div>
+
+    <div class="gameboardcontainer">
+        {#if schedulearray.length === 0}
+            <div class="nogame">해당 일자에 경기가 없습니다.</div>
+        {/if}
+        {#each schedulearray as schedule, index}
+            <div class="gameboard">
+                <div class="gameteam">
+                    <img
+                        src="/TeamLogo/{schedule['home_teamid']}.svg"
+                        alt="TeamLogo"
+                        class="teamlogo"
+                    />
+                    <div class="teamname">{schedule["home_teamname"]}</div>
+                </div>
+                <div class="score">{schedule["home_gamescore"]}</div>
+                <div class="vs">VS</div>
+                <div class="score">{schedule["away_gamescore"]}</div>
+                <div class="gameteam">
+                    <img
+                        src="/TeamLogo/{schedule['away_teamid']}.svg"
+                        alt="TeamLogo"
+                        class="teamlogo"
+                    />
+                    <div class="teamname">{schedule["away_teamname"]}</div>
+                </div>
+            </div>
+        {/each}
+    </div>
+
+    <div class="mvptitle">
+        {$store?.selected.toLocaleDateString(undefined, DateOptions)}의 MVP
+    </div>
 
     <div class="line">
         <div class="box mvpPT">
             <div class="title">득점</div>
-            <div>1. Nikola Jokic DEN 28</div>
-            <div>2. JImmy Butler MIA 21</div>
-            <div>3. Bam Adebayo MIA 20</div>
-            <div>4. Michael Porter Jr. DEN 16</div>
-            <div>5. Jamal Murray  DEN 14</div>
+            <table class="ranktable">
+                {#each RankArray_PTS as Rank}
+                    <tr>
+                        <td>{Rank[0]}</td>
+                        <td>{Rank[1]}</td>
+                        <td>{Rank[2]}</td>
+                        <td>{Rank[3]}</td>
+                    </tr>
+                {/each}
+            </table>
         </div>
         <div class="box mvpREB">
             <div class="title">리바운드</div>
-            <div>1. Nikola Jokic DEN 28</div>
-            <div>2. JImmy Butler MIA 21</div>
-            <div>3. Bam Adebayo MIA 20</div>
-            <div>4. Michael Porter Jr. DEN 16</div>
-            <div>5. Jamal Murray  DEN 14</div>
+            <table class="ranktable">
+                {#each RankArray_REB as Rank}
+                    <tr>
+                        <td>{Rank[0]}</td>
+                        <td>{Rank[1]}</td>
+                        <td>{Rank[2]}</td>
+                        <td>{Rank[3]}</td>
+                    </tr>
+                {/each}
+            </table>
         </div>
         <div class="box mvpAST">
             <div class="title">어시스트</div>
-            <div>1. Nikola Jokic DEN 28</div>
-            <div>2. JImmy Butler MIA 21</div>
-            <div>3. Bam Adebayo MIA 20</div>
-            <div>4. Michael Porter Jr. DEN 16</div>
-            <div>5. Jamal Murray  DEN 14</div>
+            <table class="ranktable">
+                {#each RankArray_AST as Rank}
+                    <tr>
+                        <td>{Rank[0]}</td>
+                        <td>{Rank[1]}</td>
+                        <td>{Rank[2]}</td>
+                        <td>{Rank[3]}</td>
+                    </tr>
+                {/each}
+            </table>
         </div>
     </div>
-    
+
     <div class="line">
         <div class="box mvpBLK">
             <div class="title">블록</div>
-            <div>1. Nikola Jokic DEN 28</div>
-            <div>2. JImmy Butler MIA 21</div>
-            <div>3. Bam Adebayo MIA 20</div>
-            <div>4. Michael Porter Jr. DEN 16</div>
-            <div>5. Jamal Murray  DEN 14</div>
+            <table class="ranktable">
+                {#each RankArray_BLK as Rank}
+                    <tr>
+                        <td>{Rank[0]}</td>
+                        <td>{Rank[1]}</td>
+                        <td>{Rank[2]}</td>
+                        <td>{Rank[3]}</td>
+                    </tr>
+                {/each}
+            </table>
         </div>
         <div class="box mvpSTL">
             <div class="title">스틸</div>
-            <div>1. Nikola Jokic DEN 28</div>
-            <div>2. JImmy Butler MIA 21</div>
-            <div>3. Bam Adebayo MIA 20</div>
-            <div>4. Michael Porter Jr. DEN 16</div>
-            <div>5. Jamal Murray  DEN 14</div>
+            <table class="ranktable">
+                {#each RankArray_STL as Rank}
+                    <tr>
+                        <td>{Rank[0]}</td>
+                        <td>{Rank[1]}</td>
+                        <td>{Rank[2]}</td>
+                        <td>{Rank[3]}</td>
+                    </tr>
+                {/each}
+            </table>
         </div>
         <div class="box mvpFTP">
             <div class="title">판타지 포인트</div>
-            <div>1. Nikola Jokic DEN 28</div>
-            <div>2. JImmy Butler MIA 21</div>
-            <div>3. Bam Adebayo MIA 20</div>
-            <div>4. Michael Porter Jr. DEN 16</div>
-            <div>5. Jamal Murray  DEN 14</div>
-        </div> 
+            <table class="ranktable">
+                {#each RankArray_FANTASY_PTS as Rank}
+                    <tr>
+                        <td>{Rank[0]}</td>
+                        <td>{Rank[1]}</td>
+                        <td>{Rank[2]}</td>
+                        <td>{Rank[3]}</td>
+                    </tr>
+                {/each}
+            </table>
+        </div>
     </div>
-</div>
-    
+</body>
+
 <style>
-    .date{
-        display: inline-flex;
-        width: 150px;
-        height: 60px;
-        background-color: skyblue;
-        line-height: 60px;
-        justify-content: center;
-        font-weight: 18px;
-        font-size: 15px;
-        margin: 30px;
-        border-radius: 10px;
+    body {
+        overflow: auto;
+        /* height: calc(100vh - 50px); */
+        width: 100%;
+        height: 100%;
+        background-color: rgb(246, 246, 246);
     }
 
-    .week{
-        margin: 30px auto;
-        text-align: center;
+    .homeimage01 {
+        width: 100%;
     }
-    
-    .line{
+
+    .datepickercontainer {
+        display: grid;
+        /* 달력이 Datepicker를 중심으로 커져서 윗쪽에 여유가 없으면 짤려서 표시된다.*/
+        /* layout 뒤에 가려지지 않고 달력이 body 영역 안에서만 나오게 처리할수 있으면 좋겠다*/
+        /* padding-top: 250px; */
+        padding: 30px;
+        justify-content: center;
+    }
+
+    .gameboardcontainer {
+        display: flex;
+        border: 1px solid black;
+        border-radius: 5px;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin: 0px 100px;
+        background-color: white;
+    }
+
+    .nogame {
+        width: 100%;
+        text-align: center;
+        font-size: 30px;
+        font-weight: bolder;
+        padding-top: 30px;
+        padding-bottom: 30px;
+    }
+
+    .gameboard {
+        width: 44%;
+        height: 150px;
+        border: 1px solid black;
+        border-radius: 10px;
+        margin: 3%;
+
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .gameteam {
+        width: 25%;
+        height: 90%;
+        /* border: 1px solid blue; */
+        margin: auto 0px;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .teamlogo {
+        width: 70%;
+    }
+    .teamname {
+        font-size: 15px;
+        font-weight: bolder;
+        text-align: center;
+        width: 100%;
+    }
+
+    .score {
+        width: 20%;
+        font-size: 40px;
+        text-align: center;
+        margin: auto 0px;
+    }
+
+    .vs {
+        font-size: 20px;
+        margin: auto 0px;
+    }
+
+    .line {
         display: flex;
     }
 
-    .title{
-        font-size: 20px;
+    .title {
+        font-size: 22px;
         font-weight: bold;
+        padding-top: 5px;
+        padding-bottom: 5px;
     }
 
-    .box{
-        width: 300px;
-        height: 180px;
+    .box {
+        width: 350px;
+        height: 200px;
         margin: 30px auto;
         text-align: center;
         border-radius: 30px;
+        border-radius: 50px;
+        font-size: 18px;
     }
 
-    .day{
-        display: inline-flex;
-        width: 70px;
-        height: 60px;
-        font-weight: 400;
-        font-size: 20px;
-        text-align: center;
-        line-height: 60px;
-        justify-content: center;
-    }
-
-    tr{
-        border: 1px solid black;
-    }
-
-    .sunday{
-        color: red;
-    }
-
-    .saturday{
-        color: blue;
-    }
-
-    .mvptitle{
+    .mvptitle {
         margin: 20px auto;
-        width: 200px;
+        width: 400px;
         height: 100px;
         line-height: 100px;
-        font-family: 'Inter';
+        font-family: "Inter";
         font-weight: bold;
         text-align: center;
         justify-content: center;
         font-size: 25px;
-        background-color: #FCCEE2;
+        background-color: #fccee2;
         border-radius: 20px;
     }
 
-    .mvpPT{
-        text-align: center;
-        background: #FDB7BA;
-        border-radius: 50px;
+    .mvpPT {
+        background: #fdb7ba;
     }
 
-    .mvpREB{
-        text-align: center;
-        background: #F3CDAD;
-        border-radius: 50px;
+    .mvpREB {
+        background: #f3cdad;
     }
 
-    .mvpAST{
-        text-align: center;
-        background: #FCFFB0;
-        border-radius: 50px;
+    .mvpAST {
+        background: #fcffb0;
     }
 
-    .mvpBLK{
-        text-align: center;
-        background: #AFFFBA;
-        border-radius: 50px;
-    }
-    
-    .mvpSTL{
-        text-align: center;
-        background: #98BBEB;
-        border-radius: 50px;
+    .mvpBLK {
+        background: #afffba;
     }
 
-    .mvpFTP{
-        text-align: center;
-        background: #E2B9FF;
-        border-radius: 50px;
+    .mvpSTL {
+        background: #98bbeb;
     }
 
-    .day:hover{
-        background-color: rgb(224, 202, 202);
-        cursor: pointer;
-    }
-    .sunday:hover{
-        background-color: rgb(224, 202, 202);
-        cursor: pointer;
-    }
-    .saturday:hover{
-        background-color: rgb(224, 202, 202);
-        cursor: pointer;
+    .mvpFTP {
+        background: #e2b9ff;
     }
 
-    .day:active{
-        background-color: rgb(145, 172, 223);
-        cursor: pointer;
-    }
-    .sunday:active{
-        background-color: rgb(145, 172, 223);
-        cursor: pointer;
-    }
-    .saturday:active{
-        background-color: rgb(145, 172, 223);
-        cursor: pointer;
+    .ranktable {
+        width: 90%;
+        margin: 5px 5%;
     }
 </style>
