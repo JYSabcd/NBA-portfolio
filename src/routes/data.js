2d0d77c54d539586d43525505e38976d3c598771
JYS <jys60mlb@gmail.com>
2023-09-10 22:49:39 +0900

    TeamObj

---
+++ src/routes/data.js
@@ -0,0 +1,228 @@
+export const ScheduleYYYYMMArray = [
+'2016-10','2016-11','2016-12',
+'2017-01','2017-02','2017-03','2017-04','2017-05','2017-06','2017-09','2017-10','2017-11','2017-12',
+'2018-01','2018-02','2018-03','2018-04','2018-05','2018-06','2018-09','2018-10','2018-11','2018-12',
+'2019-01','2019-02','2019-03','2019-04','2019-05','2019-06','2019-09','2019-10','2019-11','2019-12',
+'2020-01','2020-02','2020-03','2020-07','2020-08','2020-09','2020-10','2020-12','2021-01','2021-02',
+'2021-03','2021-04','2021-05','2021-06','2021-07','2021-10','2021-11','2021-12',
+'2022-01','2022-02','2022-03','2022-04','2022-05','2022-06','2022-09','2022-10','2022-11','2022-12',
+'2023-01','2023-02','2023-03','2023-04','2023-05','2023-06'
+];
+
+export const TeamnameArray = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls", "Cleveland Cavaliers",
+"Dallas Mavericks", "Denver Nuggets", "Detroit Pistons", "Golden State Warriors", "Houston Rockets", "Indiana Pacers", "Los Angeles Clippers",
+"Los Angeles Lakers", "Memphis Grizzlies", "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans","New York Knicks",
+"Oklahoma City Thunder", "Orlando Magic", "Philadelphia 76ers", "Phoenix Suns", "Portland Trail Blazers", "Sacramento Kings", "San Antonio Spurs",
+"Toronto Raptors", "Utah Jazz", "Washington Wizards"];
+
+export const TeamObj = {
+    "Atlanta Hawks": {
+        TeamID: 1610612737,
+        Abbrev: "ATL",
+        TeamName: "Hawks",
+        City: "Atlanta",
+    },
+
+    "Boston Celtics": {
+        TeamID: 1610612738,
+        Abbrev: "BOS",
+        TeamName: "Celtics",
+        City: "Boston",
+    },
+
+    "Brooklyn Nets": {
+        TeamID: 1610612751,
+        Abbrev: "BKN",
+        TeamName: "Nets",
+        City: "Brooklyn",
+    },
+
+    "Charlotte Hornets": {
+        TeamID: 1610612766,
+        Abbrev: "CHA",
+        TeamName: "Hornets",
+        City: "Charlotte",
+    },
+
+    "Chicago Bulls": {
+        TeamID: 1610612741,
+        Abbrev: "CHI",
+        TeamName: "Bulls",
+        City: "Chicago",
+    },
+
+    "Cleveland Cavaliers": {
+        TeamID: 1610612739,
+        Abbrev: "CLE",
+        TeamName: "Cavaliers",
+        City: "Cleveland",
+    },
+
+    "Dallas Mavericks": {
+        TeamID: 1610612742,
+        Abbrev: "DAL",
+        TeamName: "Mavericks",
+        City: "Dallas",
+    },
+
+    "Denver Nuggets": {
+        TeamID: 1610612743,
+        Abbrev: "DEN",
+        TeamName: "Nuggets",
+        City: "Denver",
+    },
+
+    "Detroit Pistons": {
+        TeamID: 1610612765,
+        Abbrev: "DET",
+        TeamName: "Pistons",
+        City: "Detroit",
+    },
+
+    "Golden State Warriors": {
+        TeamID: 1610612744,
+        Abbrev: "GSW",
+        TeamName: "Warriors",
+        City: "Golden State",
+    },
+
+    "Houston Rockets": {
+        TeamID: 1610612745,
+        Abbrev: "HOU",
+        TeamName: "Rockets",
+        City: "Houston",
+    },
+
+    "Indiana Pacers": {
+        TeamID: 1610612754,
+        Abbrev: "IND",
+        TeamName: "Pacers",
+        City: "Indiana",
+    },
+
+    "Los Angeles Clippers": {
+        TeamID: 1610612746,
+        Abbrev: "LAC",
+        TeamName: "Clippers",
+        City: "Los Angeles",
+    },
+
+    "Los Angeles Lakers": {
+        TeamID: 1610612747,
+        Abbrev: "LAL",
+        TeamName: "Lakers",
+        City: "Los Angeles",
+    },
+
+    "Memphis Grizzlies": {
+        TeamID: 1610612763,
+        Abbrev: "MEM",
+        TeamName: "Grizzlies",
+        City: "Memphis",
+    },
+
+    "Miami Heat": {
+        TeamID: 1610612748,
+        Abbrev: "MIA",
+        TeamName: "Heat",
+        City: "Miami",
+    },
+
+    "Milwaukee Bucks": {
+        TeamID: 1610612749,
+        Abbrev: "MIL",
+        TeamName: "Bucks",
+        City: "Milwaukee",
+    },
+
+    "Minnesota Timberwolves": {
+        TeamID: 1610612750,
+        Abbrev: "MIN",
+        TeamName: "Timberwolves",
+        City: "Minnesota",
+    },
+
+    "New Orleans Pelicans": {
+        TeamID: 1610612740,
+        Abbrev: "NOP",
+        TeamName: "Pelicans",
+        City: "New Orleans",
+    },
+
+    "New York Knicks": {
+        TeamID: 1610612752,
+        Abbrev: "NYK",
+        TeamName: "Knicks",
+        City: "New York",
+    },
+
+    "Oklahoma City Thunder": {
+        TeamID: 1610612760,
+        Abbrev: "OKC",
+        TeamName: "Thunder",
+        City: "Oklahoma City",
+    },
+
+    "Orlando Magic": {
+        TeamID: 1610612753,
+        Abbrev: "ORL",
+        TeamName: "Magic",
+        City: "Orlando",
+    },
+
+    "Philadelphia 76ers": {
+        TeamID: 1610612755,
+        Abbrev: "PHI",
+        TeamName: "76ers",
+        City: "Philadelphia",
+    },
+
+    "Phoenix Suns": {
+        TeamID: 1610612756,
+        Abbrev: "PHX",
+        TeamName: "Suns",
+        City: "Phoenix",
+    },
+
+    "Portland Trail Blazers": {
+        TeamID: 1610612757,
+        Abbrev: "POR",
+        TeamName: "Trail Blazers",
+        City: "Portland",
+    },
+
+    "Sacramento Kings": {
+        TeamID: 1610612758,
+        Abbrev: "SAC",
+        TeamName: "Kings",
+        City: "Sacramento",
+    },
+
+    "San Antonio Spurs": {
+        TeamID: 1610612759,
+        Abbrev: "SAS",
+        TeamName: "Spurs",
+        City: "San Antonio",
+    },
+
+    "Toronto Raptors": {
+        TeamID: 1610612761,
+        Abbrev: "TOR",
+        TeamName: "Raptors",
+        City: "Toronto",
+    },
+
+    "Utah Jazz": {
+        TeamID: 1610612762,
+        Abbrev: "UTA",
+        TeamName: "Jazz",
+        City: "Utah",
+    },
+
+    "Washington Wizards": {
+        TeamID: 1610612764,
+        Abbrev: "WAS",
+        TeamName: "Wizards",
+        City: "Washington",
+    },
+};
