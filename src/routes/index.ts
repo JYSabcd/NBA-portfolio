48555fcb83524cf1ce39612b8e7bbbd6cc8fe15b
JYS <jys60mlb@gmail.com>
2023-10-21 14:23:07 +0900

    home 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,12 +2,14 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import compareplayer from './compareplayer'
+import home from './home'
 const app = new Hono()
 app.use('*', cors())
 
 app.get('/', (c) => c.text('Hello Hono!'))
 
 app.route('/compareplayer', compareplayer)
+app.route('/home', home)
 
 serve({
     fetch:app.fetch,
