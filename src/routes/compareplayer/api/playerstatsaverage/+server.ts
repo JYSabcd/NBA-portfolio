b9a110a504c106a1991349c0a5960650a33e6b39
JYS <jys60mlb@gmail.com>
2023-10-21 15:12:25 +0900

    서버 원래 코드로 수정

--- src/routes/compareplayer/api/playerstatsaverage/+server.ts
+++ src/routes/compareplayer/api/playerstatsaverage/+server.ts
@@ -1,108 +1,74 @@
-// import { json } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit';
 
-// const headersList = {
-//     'Host': 'stats.nba.com',
-//     'Referer': 'https://www.nba.com',
-//     'Origin': 'https://stats.nba.com/',
-//     'x-nba-stats-origin': 'stats',
-//     'x-nba-stats-token': 'true',
-//     'Cache-Control': 'max-age=0',
-//     'Upgrade-Insecure-Requests': '1',
-//     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0',
-//     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3',
-//     'Accept-Encoding': 'gzip, deflate, br',
-//     'Accept-Language': 'en-US,en;q=0.9',
-//     };
+const headersList = {
+    'Host': 'stats.nba.com',
+    'Referer': 'https://www.nba.com',
+    'Origin': 'https://stats.nba.com/',
+    'x-nba-stats-origin': 'stats',
+    'x-nba-stats-token': 'true',
+    'Cache-Control': 'max-age=0',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    };
 
-// const options = {
-//     method: 'GET',
-//     headers: headersList
-// };
+const options = {
+    method: 'GET',
+    headers: headersList
+};
 
-// /** @type {import('./$types').RequestHandler} */
-// export async function GET({url}) {
-//     let URL = "https://stats.nba.com/stats/leaguedashplayerstats?LastNGames=0&LeagueID=00&MeasureType=Base&Month=0&OpponentTeamID=0&PaceAdjust=N&Period=0&PerMode=PerGame&PlusMinus=N&PORound=0&Rank=N&SeasonType=Regular+Season&TeamID=0&TwoWay=0"
-//     +'&Season=' + url.searchParams.get('Season')
-//     const res = await fetch(URL, options);
-//     const ResJson = await res.json();
-
-//     // console.log(ResJson);
-//     //console.log(ResJson.resultSets[0].headers);
-//     // console.log(ResJson.resultSets[0].rowSet);
-
-//     /** @type {(number|string)[][]} */
-//     let rowSet = ResJson.resultSets[0].rowSet;
-
-//     let PlayerCount = 0;
-//     let accMIN = 0;
-//     let accPTS = 0;
-//     let accREB = 0;
-//     let accAST = 0;
-//     let accSTL = 0;
-//     let accBLK = 0;
-
-//     for(let i = 0; i < rowSet.length; i++){
-//         if(rowSet[i][6] < 58){ // 58경기 미만인 선수들 버림
-//             continue;
-//         }
-
-//         accMIN += rowSet[i][10]; // MIN
-//         accPTS += rowSet[i][30]; // PTS
-//         accREB += rowSet[i][22]; // REB
-//         accAST += rowSet[i][23]; // AST
-//         accSTL += rowSet[i][25]; // STL
-//         accBLK += rowSet[i][26]; // BLK
-//         PlayerCount += 1; // 선수가 들어올 때 한명씩 더함
-//     }
-
-//     let ReturnData = [];
-
-//     if(PlayerCount === 0){
-//         ReturnData = [0,0,0,0,0,0];
-//     }
-//     else{
-//         ReturnData = [
-//             (accMIN/PlayerCount).toFixed(1),
-//             (accPTS/PlayerCount).toFixed(1),
-//             (accREB/PlayerCount).toFixed(1),
-//             (accAST/PlayerCount).toFixed(1),
-//             (accSTL/PlayerCount).toFixed(1),
-//             (accBLK/PlayerCount).toFixed(1)
-//         ]
-//     }
-
-//     return json(ReturnData);
-
-import { Hono } from 'hono'
-import { headersOption } from './setting'
-
-const api = new Hono()
-
-api.get('/api/playerstats', async c => {
-    const reqUrl = new URL(c.req.url)
-    const url = new URL("https://stats.nba.com/stats/leaguedashplayerstats?LastNGames=0&LeagueID=00&MeasureType=Base&Month=0&OpponentTeamID=0&PaceAdjust=N&Period=0&PerMode=PerGame&PlusMinus=N&PORound=0&Rank=N&SeasonType=Regular+Season&TeamID=0&TwoWay=0")
-    url.searchParams.append('Season', reqUrl.searchParams.get('Season') ?? '')
-    const res = await fetch(url, headersOption);
+/** @type {import('./$types').RequestHandler} */
+export async function GET({url}) {
+    let URL = "https://stats.nba.com/stats/leaguedashplayerstats?LastNGames=0&LeagueID=00&MeasureType=Base&Month=0&OpponentTeamID=0&PaceAdjust=N&Period=0&PerMode=PerGame&PlusMinus=N&PORound=0&Rank=N&SeasonType=Regular+Season&TeamID=0&TwoWay=0"
+    +'&Season=' + url.searchParams.get('Season')
+    const res = await fetch(URL, options);
     const ResJson = await res.json();
-    const rowSet:(number|string)[][] = ResJson.resultSets[0].rowSet;
+
+    // console.log(ResJson);
+    //console.log(ResJson.resultSets[0].headers);
+    // console.log(ResJson.resultSets[0].rowSet);
+
+    /** @type {(number|string)[][]} */
+    let rowSet = ResJson.resultSets[0].rowSet;
 
     let PlayerCount = 0;
-    const ReturnData:number[] = [0, 0, 0, 0, 0, 0];
+    let accMIN = 0;
+    let accPTS = 0;
+    let accREB = 0;
+    let accAST = 0;
+    let accSTL = 0;
+    let accBLK = 0;
+
     for(let i = 0; i < rowSet.length; i++){
-        if(Number(rowSet[i][6]) < 58){ // 58경기 미만인 선수들 버림
+        if(rowSet[i][6] < 58){ // 58경기 미만인 선수들 버림
             continue;
         }
 
-        ReturnData[0] += Number(rowSet[i][10]); // MIN
-        ReturnData[1] += Number(rowSet[i][30]); // PTS
-        ReturnData[2] += Number(rowSet[i][22]); // REB
-        ReturnData[3] += Number(rowSet[i][23]); // AST
-        ReturnData[4] += Number(rowSet[i][25]); // STL
-        ReturnData[5] += Number(rowSet[i][26]); // BLK
+        accMIN += rowSet[i][10]; // MIN
+        accPTS += rowSet[i][30]; // PTS
+        accREB += rowSet[i][22]; // REB
+        accAST += rowSet[i][23]; // AST
+        accSTL += rowSet[i][25]; // STL
+        accBLK += rowSet[i][26]; // BLK
         PlayerCount += 1; // 선수가 들어올 때 한명씩 더함
     }
 
-    return c.json(ReturnData.map(v => v.toFixed(1)));
-})
+    let ReturnData = [];
 
-export default api
+    if(PlayerCount === 0){
+        ReturnData = [0,0,0,0,0,0];
+    }
+    else{
+        ReturnData = [
+            (accMIN/PlayerCount).toFixed(1),
+            (accPTS/PlayerCount).toFixed(1),
+            (accREB/PlayerCount).toFixed(1),
+            (accAST/PlayerCount).toFixed(1),
+            (accSTL/PlayerCount).toFixed(1),
+            (accBLK/PlayerCount).toFixed(1)
+        ]
+    }
+
+    return json(ReturnData);
--- src/routes/home/db/getschedule/+server.ts
+++ src/routes/home/db/getschedule/+server.ts
@@ -1,83 +1,62 @@
-// import { json } from '@sveltejs/kit';
-// import pg from "pg";
-// const { Pool } = pg;
-
-// //import { native as pg } from 'pg';
-
-// // const config = {
-// //     host: 'localhost',
-// //     // Do not hard code your username and password.
-// //     // Consider using Node environment variables.
-// //     user: 'postgres',
-// //     password: 'sqlpass1!',
-// //     database: 'NBADB',
-// //     port: 5432,
-// //     ssl: false
-// // };
-
-// // const config = {
-// //     host: 'nba-project.kro.kr',
-// //     // Do not hard code your username and password.
-// //     // Consider using Node environment variables.
-// //     user: 'jys0521',
-// //     password: 'abcdxyz',
-// //     database: 'nbadb',
-// //     port: 5432,
-// //     ssl: false
-// // };
-
-// import {env} from '$env/dynamic/private';
-// const {
-//     DBconfig_host,
-//     DBconfig_user,
-//     DBconfig_password,
-//     DBconfig_database,
-//     DBconfig_port
-// } = env;
-
-
-// const config = {
-//     host: DBconfig_host,
-//     user: DBconfig_user,
-//     password: DBconfig_password,
-//     database: DBconfig_database,
-//     port: DBconfig_port,
-//     ssl: false
-// };
-
-// /** @type {import('./$types').RequestHandler} */
-// export async function GET({ url }) {
-
-//     const pool = new Pool(config);
-
-//     const connectToDB = await pool.connect();
-
-//     let query = "select * from public.game_schedule where game_date = '" + url.searchParams.get("SelectedDate") + "';";
-
-//     const res = await connectToDB.query(query);
-
-//     connectToDB.release();
-    
-//     return json(res.rows);
-// }
-import { Hono } from "hono";
-import { headersOption, pool } from "./setting";
+import { json } from '@sveltejs/kit';
 import pg from "pg";
 const { Pool } = pg;
 
-const api = new Hono()
+//import { native as pg } from 'pg';
 
-api.get('/db/getschedule', async c => {
-    const url = new URL(c.req.url)
+// const config = {
+//     host: 'localhost',
+//     // Do not hard code your username and password.
+//     // Consider using Node environment variables.
+//     user: 'postgres',
+//     password: 'sqlpass1!',
+//     database: 'NBADB',
+//     port: 5432,
+//     ssl: false
+// };
+
+// const config = {
+//     host: 'nba-project.kro.kr',
+//     // Do not hard code your username and password.
+//     // Consider using Node environment variables.
+//     user: 'jys0521',
+//     password: 'abcdxyz',
+//     database: 'nbadb',
+//     port: 5432,
+//     ssl: false
+// };
+
+import {env} from '$env/dynamic/private';
+const {
+    DBconfig_host,
+    DBconfig_user,
+    DBconfig_password,
+    DBconfig_database,
+    DBconfig_port
+} = env;
+
+
+const config = {
+    host: DBconfig_host,
+    user: DBconfig_user,
+    password: DBconfig_password,
+    database: DBconfig_database,
+    port: DBconfig_port,
+    ssl: false
+};
+
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+
+    const pool = new Pool(config);
+
     const connectToDB = await pool.connect();
 
-    let query = `select * from public.game_schedule where game_date = '${url.searchParams.get("SelectedDate")}';`;
+    let query = "select * from public.game_schedule where game_date = '" + url.searchParams.get("SelectedDate") + "';";
 
     const res = await connectToDB.query(query);
 
     connectToDB.release();
     
-    return c.json(res.rows);
-})
-
-export default api
+    return json(res.rows);
+}
